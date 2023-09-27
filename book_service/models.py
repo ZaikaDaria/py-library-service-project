1f2f70ee1f2b8fa7be71b820c70e7ff55362ad2c
ZaikaDaria <corplinebusiness@gmail.com>
2023-09-27 22:58:14 +0300

    Add Book model to boor_service.models.py

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.utils.text import slugify
+
+
+def book_cover_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads", "books", filename)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.ImageField(null=True, upload_to=book_cover_file_path, blank=True)
+    inventory = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = "book"
+        verbose_name_plural = "books"
+
+    def __str__(self):
+        return f"{self.title} {self.author}"
