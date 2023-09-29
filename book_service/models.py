62b3923d949d8082c8ba84167b9a3c9ef666ef40
ZaikaDaria <corplinebusiness@gmail.com>
2023-09-29 23:32:57 +0300

    Add author=settings.AUTH_USER_MODEL to Book model

--- book_service/models.py
+++ book_service/models.py
@@ -1,8 +1,9 @@
 import os
 import uuid
 
-from django.core.validators import MinValueValidator
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.utils.text import slugify
 
 
@@ -15,7 +16,12 @@ def book_cover_file_path(instance, filename):
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="books",
+        unique=True,
+    )
     cover = models.ImageField(null=True, upload_to=book_cover_file_path, blank=True)
     inventory = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
