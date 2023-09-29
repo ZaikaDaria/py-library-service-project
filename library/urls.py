28698f3b025f0dfde383a5a5f567f3a307ea9ef5
ZaikaDaria <corplinebusiness@gmail.com>
2023-09-29 23:35:47 +0300

    Create book_service.urls registration in the main urls.py

--- library/urls.py
+++ library/urls.py
@@ -15,8 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+# from drf_spectacular.views import (
+#     SpectacularAPIView,
+#     SpectacularRedocView,
+#     SpectacularSwaggerView,
+# )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("api/", include("book_service.urls", namespace="book_service")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
