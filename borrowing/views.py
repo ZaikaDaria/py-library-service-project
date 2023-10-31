bc43ec2dad5f7d15b82268a911764bb6fff742b7
ZaikaDaria <corplinebusiness@gmail.com>
2023-10-31 20:54:53 +0200

    Implement borrowing view

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Borrowing
+from .serializers import BorrowingSerializer
 
-# Create your views here.
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
