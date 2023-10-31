e5023f9e6b757283196091428b5fcd007b627995
ZaikaDaria <corplinebusiness@gmail.com>
2023-10-31 20:53:37 +0200

    Add urls for Borrowing views

---
+++ borrowing/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from .views import BorrowingViewSet
+
+router = DefaultRouter()
+router.register("borrowings", BorrowingViewSet)
+
+urlpatterns = router.urls
+
+app_name = "borrowing"
