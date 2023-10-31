5f7cce74b359169b3bb95a9f22d54e340d3f7bcb
ZaikaDaria <corplinebusiness@gmail.com>
2023-10-31 21:15:14 +0200

    Implement create method in views.py

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,16 @@
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status, viewsets
 from .models import Borrowing
-from .serializers import BorrowingSerializer
+from .serializers import CreateBorrowingSerializer, BorrowingSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = CreateBorrowingSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
