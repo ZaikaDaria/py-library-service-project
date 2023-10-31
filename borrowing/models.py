d659839ec3e229b04b80db5b870008c2da17d275
ZaikaDaria <corplinebusiness@gmail.com>
2023-10-31 20:52:29 +0200

    Add borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+
+class Borrowing(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
