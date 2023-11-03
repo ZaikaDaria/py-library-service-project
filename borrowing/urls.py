f18fc8e9205ec92cb59850872ef40c9c1b880e50
ZaikaDaria <corplinebusiness@gmail.com>
2023-11-04 00:39:28 +0200

    Implement return borrowing logic

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,9 +1,13 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BorrowingViewSet
+from .views import BorrowingViewSet, ReturnBorrowingViewSet
 
 router = DefaultRouter()
 router.register("borrowings", BorrowingViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api/borrowings/return/', ReturnBorrowingViewSet.as_view({"post": "create"}), name="return-borrowing"),
+]
 
 app_name = "borrowing"
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from .filters import BorrowingFilter
@@ -21,3 +22,25 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         if self.request.user.is_staff:
             return Borrowing.objects.all()
         return Borrowing.objects.filter(user=self.request.user)
+
+
+class ReturnBorrowingViewSet(viewsets.ViewSet):
+    def create(self, request, *args, **kwargs):
+        borrowing_id = request.data.get("borrowing_id")
+        try:
+            borrowing = Borrowing.objects.get(id=borrowing_id)
+        except Borrowing.DoesNotExist:
+            return Response({"detail": "Borrowing not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        if borrowing.actual_return_date:
+            return Response({"detail": "Borrowing has already been returned"}, status=status.HTTP_400_BAD_REQUEST)
+
+        borrowing.actual_return_date = timezone.now()
+        borrowing.save()
+
+        # Increase book inventory by 1
+        borrowing.book.inventory += 1
+        borrowing.book.save()
+
+        serializer = BorrowingSerializer(borrowing)
+        return Response(serializer.data, status=status.HTTP_200_OK)
