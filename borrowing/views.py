9e1b21181c3c30ba47d181479f722a276c6a6093
ZaikaDaria <corplinebusiness@gmail.com>
2023-11-04 00:24:09 +0200

    Add customer filter to views.py

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from .filters import BorrowingFilter
 from .models import Borrowing
 from .serializers import CreateBorrowingSerializer, BorrowingSerializer
 
@@ -7,6 +8,7 @@ from .serializers import CreateBorrowingSerializer, BorrowingSerializer
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    filter_class = BorrowingFilter
 
     def create(self, request, *args, **kwargs):
         serializer = CreateBorrowingSerializer(data=request.data, context={"request": request})
@@ -14,3 +16,8 @@ class BorrowingViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Borrowing.objects.all()
+        return Borrowing.objects.filter(user=self.request.user)
