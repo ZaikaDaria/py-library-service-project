e428c577f2a1fdca6c4e9770a8f487ca4140d065
ZaikaDaria <corplinebusiness@gmail.com>
2023-11-08 00:52:00 +0200

    Add Payment model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -11,3 +12,12 @@ class Borrowing(models.Model):
     borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateField()
+
+    def __str__(self):
+        return f"Payment ID: {self.id}"
