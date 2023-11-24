71c4be6a14ce606ded708d99728bb41491df79bc
ZaikaDaria <corplinebusiness@gmail.com>
2023-11-24 17:20:01 +0200

    Fix borrowing/url bugs

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,16 +1,12 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import BorrowingViewSet, ReturnBorrowingViewSet
+from .views import BorrowingViewSet
 
 router = DefaultRouter()
 router.register("borrowings", BorrowingViewSet)
 
 urlpatterns = [
-    path(
-        "api/borrowings/return/",
-        ReturnBorrowingViewSet.as_view({"post": "create"}),
-        name="return-borrowing",
-    ),
+    path("", include(router.urls))
 ] + router.urls
 
 app_name = "borrowing"
